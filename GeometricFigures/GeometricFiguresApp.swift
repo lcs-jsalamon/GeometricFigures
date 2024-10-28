e7bb3509f68c7a5b8e970461e772c9bda89ba647
Josephine Salamon <jsalamon@lcs.on.ca>
2024-10-28 09:47:45 -0400

    Initial Commit

---
+++ GeometricFigures/GeometricFiguresApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricFiguresApp.swift
+//  GeometricFigures
+//
+//  Created by Josephine Salamon on 2024-10-28.
+//
+
+import SwiftUI
+
+@main
+struct GeometricFiguresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
